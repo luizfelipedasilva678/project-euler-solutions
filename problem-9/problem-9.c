a2ac77d7f7cb431fb96282cc75a1fb813473dfb8
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2024-05-02 16:30:30 -0300

    Problem 9

---
+++ problem-9/problem-9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// TODO: Improve algorithm
+
+int special_pythagorean_triplet()
+{
+    int a, b, c, result = 0;
+
+    for (a = 1; a < 1000; a++)
+    {
+        for (b = 2; b < 1000; b++)
+        {
+            for (c = 3; c < 1000; c++)
+            {
+                if (a * a + b * b == c * c && a + b + c == 1000)
+                {
+                    result = a * b * c;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int main(void)
+{
+    printf("%d\n", special_pythagorean_triplet());
+
+    return 0;
+}
